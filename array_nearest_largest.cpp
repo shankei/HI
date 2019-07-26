80fbb5a1df0b86810db61c6efbad444c0038c956
shankei <shashankagarwal2310@.com>
2019-07-26 15:52:13 +0530

    array nearest largest

---
+++ array_nearest_largest.cpp
@@ -0,0 +1,33 @@
+// given an array calculate array r such that r[i]=min j such that r[j]>=r[i];
+// r= position of nearest greatest no.to right side.
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n,i;
+cout<<"enter n"<<endl;
+   cin >>n;
+
+	int arr[n],s[n],R[n];
+	cout <<"enter array"<<endl;
+
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+
+	int top=-1;
+	for(i=0;i<n;i++)
+	{
+		while(top>-1&&arr[s[top]]<=arr[i])
+		R[s[top--]]=i;
+
+		s[++top]=i;
+
+	}
+	while(top>-1)
+	R[s[top--]]=n;
+	cout<<"\n";
+	for(i=0;i<n;i++)
+	cout<<R[i]<<" ";
+	return 0;
+}
